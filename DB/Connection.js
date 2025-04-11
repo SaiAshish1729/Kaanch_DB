78ccfdfe5724b54246aa67dfb5e5080927e9a80c
Souvikjr <souvikmiya1996@gmail.com>
2025-04-11 17:14:49 +0530

    minor change

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
 const Connection = async () => {
-    const URL = "mongodb://127.0.0.1:27017/Kaanch_DB"
+    // const URL = "mongodb://127.0.0.1:27017/Kaanch_DB"
     // const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.iicjf2c.mongodb.net/Kaanch-DB`
 
-
     try {
-        await mongoose.connect(URL,
+        await mongoose.connect(process.env.URL,
             // { useNewUrlParser: true }
         )
         console.log("Database Connected Successfully!");
